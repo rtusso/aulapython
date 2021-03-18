06ac2faa1445fa2ba0a865d4161b31717f6a9aa8
rtusso <rtusso@gmail.com>
2021-03-18 21:55:51 +0000

    login

--- app/routes.py
+++ app/routes.py
@@ -10,16 +10,21 @@
 #   return render_template("index.html", username="Ricardo")
 
 from app import app
-from flask import render_template
+from flask import render_template, request
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Feulo'}
-    #criar postes
     posts = [
         {'author': {'username': 'Maria'}, 'body': "Olá da Maria"},
-        {'author': {'username': 'Feulo'}, 'body': "Olá!"}
+        {'author': {'username': 'Mario'}, 'body': "Olá!"}
     ]
     return render_template("index.html", user=user, posts=posts)
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        print(request.values.get("user"), request.values.get("pass"), request.values.get("remember"))
+    return render_template("login.html" , title="Login")
