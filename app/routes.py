7671dc68acf79a9893a071312cf528b55123650c
rtusso <rtusso@gmail.com>
2021-03-18 19:30:07 +0000

    parte2

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,20 @@
+#from app import app
+##Interacao para chamar html e manda o aqruivo para frente
+#from flask import render_template
+
+
+#@app.route('/')
+#@app.route('/index')
+#def index():
+#    #return "Hello World"
+#   return render_template("index.html", username="Ricardo")
+
 from app import app
-#Interacao para chamar html e manda o aqruivo para frente
 from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    #return "Hello World"
-    return render_template("index.html")
+    user = {'username': 'Feulo'}
+    return render_template("index.html", user=user)
